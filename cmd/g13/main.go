7e25bb1207fd1d445ca43e0891b1fbfa3a9eca7e
Achilleas Koutsou <achilleas@koutsou.net>
2024-12-24 13:12:26 +0100

    g13: load config from file
    Move the built-in default config to an external file.
    Make g13 main load the config from a file specified on the command line.

--- cmd/g13/main.go
+++ cmd/g13/main.go
@@ -7,40 +7,11 @@ import (
 	"github.com/achilleas-k/g13-ak/internal/device"
 	"github.com/achilleas-k/g13-ak/internal/keyboard"
 	"github.com/achilleas-k/g13-ak/internal/mapping"
-	"github.com/bendahl/uinput"
 )
 
-var myDefaultMapping = map[device.KeyBit]int{
-	device.G1: uinput.Key1,
-	device.G2: uinput.Key2,
-	device.G3: uinput.KeyQ,
-	device.G4: uinput.KeyW,
-	device.G5: uinput.KeyE,
-	device.G6: uinput.KeyR,
-	device.G7: uinput.KeyT,
-
-	device.G8:  uinput.Key3,
-	device.G9:  uinput.Key4,
-	device.G10: uinput.KeyA,
-	device.G11: uinput.KeyS,
-	device.G12: uinput.KeyD,
-	device.G13: uinput.KeyF,
-	device.G14: uinput.KeyG,
-
-	device.G15: uinput.KeyLeftshift,
-	device.G16: uinput.KeyZ,
-	device.G17: uinput.KeyX,
-	device.G18: uinput.KeyC,
-	device.G19: uinput.KeyV,
-	device.G20: uinput.KeyLeftctrl,
-	device.G21: uinput.KeyTab,
-	device.G22: uinput.KeyLeftalt,
-
-	device.LEFT: uinput.KeySpace,
-	device.DOWN: uinput.KeyEsc,
-}
-
 func main() {
+	configPath := os.Args[1]
+
 	dev, err := device.New()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error initialising device: %s\n", err)
@@ -51,10 +22,14 @@ func main() {
 	vkb, err := keyboard.New("g13-vkb")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error initialising virtual keyboard: %s\n", err)
+		os.Exit(1)
 	}
 
-	keyMap := mapping.NewEmpty()
-	keyMap.SetKeys(myDefaultMapping)
+	keyMap, err := mapping.NewFromFile(configPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
 	fmt.Println("Ready")
 	for {
 		input, err := dev.ReadInput()
